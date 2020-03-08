22c3c83d1e73076b3ad275a21561cff7124fb6b4
CJY <cjy@CJYui-MacBook-Pro.local>
2020-03-08 20:43:46 +0900

    (jumpgame2) 느리지만 드디어 해결

--- JUMPGAME2/jumpgame2_chrisjune.py
+++ JUMPGAME2/jumpgame2_chrisjune.py
@@ -1,28 +1,36 @@
 class Solution:
-    def __init__(self):
-        self.jumpcnt = 0
-        self.result = []
+    def jump(self, nums) -> int:
+        left = 0
+        count = 0
+        while True:
+            target_list = nums[left:]
+            if len(target_list) == 1:
+                break
+            count += 1
 
-    def jump(self, nums: list) -> int:
-        if len(nums) <= 1:
-            self.result.append(self.jumpcnt)
-            return min(self.result)
+            first = target_list[0]
+            maxvalue = (-1, -1)
+            for i in range(1, min(first + 1, len(target_list))):
+                val = i + target_list[i]
+                if i >= len(target_list) - 1:
+                    maxvalue = (i, val)
+                    break
 
-        first = nums[0]
-        if first == 0:
-            return
+                if maxvalue[1] <= val:
+                    maxvalue = (i, val)
 
-        for i in range(first):
-            self.jumpcnt += 1
-            self.jump(nums[i + 1:])
-            self.jumpcnt -= 1
-        return min(self.result)
+            idx = maxvalue[0]
+            left += idx
+        return count
 
 
 if __name__ == '__main__':
     assert Solution().jump([2, 3, 1, 1, 4]) == 2
     assert Solution().jump([0]) == 0
     assert Solution().jump([1, 2]) == 1
-    assert Solution().jump([2,3,0,1,4]) == 2
-    assert Solution().jump([5,9,3,2,1,0,2,3,3,1,0,0]) == 6
-    assert Solution().jump([1,1,1,1,1,1]) == 5
+    assert Solution().jump([2, 1]) == 1
+    assert Solution().jump([3, 2, 1]) == 1
+    assert Solution().jump([2, 3, 1]) == 1
+    assert Solution().jump([2, 3, 0, 1, 4]) == 2
+    assert Solution().jump([1, 1, 1, 1, 1, 1]) == 5
+    assert Solution().jump([5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]) == 3
